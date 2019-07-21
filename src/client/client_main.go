27f16b1024ff1cacd7841db9ba38c03d32d5dd3a
Mahvash Siavashpour <mahvash-siavashpour@users.noreply.github.com>
2019-07-21 23:22:50 +0430

    modified

---
+++ src/client/client_main.go
@@ -0,0 +1,28 @@
+package client
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func RunClient() {
+
+	fmt.Println("Launching Client...")
+	connection, _ := net.Dial("tcp", "127.0.0.1:8080")
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Entre First Number:")
+	num1, _ := reader.ReadString('\n')
+	fmt.Println("Enter Operator:")
+	op, _ := reader.ReadString('\n')
+	fmt.Println("Enter Second Number:")
+	num2, _ := reader.ReadString('\n')
+	write := num1 + op + num2 + "."
+	connection.Write([]byte(write))
+
+	res, _ := bufio.NewReader(connection).ReadString('\n')
+	fmt.Println("Result:", res)
+	connection.Close()
+}
