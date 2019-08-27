cb1a828a80febba91c6e11ac659643e0647ce91d
Mahvash Siavashpour <mahvash-siavashpour@users.noreply.github.com>
2019-08-27 21:17:20 +0430

    http client and server added

--- cmd/client/http_version.go
+++ cmd/client/http_version.go
@@ -1,1 +1,54 @@
 package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type Operation struct {
+	Operand1 string
+	Operator string
+	Operand2 string
+}
+
+func main() {
+
+	url := "http://localhost:8080/"
+	//get the operator and the operands from user
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter First Number:")
+	num1, _ := reader.ReadString('\n')
+	fmt.Println("Enter Operator:")
+	op, _ := reader.ReadString('\n')
+	fmt.Println("Enter Second Number:")
+	num2, _ := reader.ReadString('\n')
+
+	operation := Operation{num1, op, num2}
+	js, err := json.Marshal(operation)
+	if err != nil {
+		fmt.Println("Error in json")
+		return
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(js))
+	req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println("response Body:", string(body))
+
+}
--- cmd/server/http_version.go
+++ cmd/server/http_version.go
@@ -1,1 +1,64 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type Operation struct {
+	Operand1 string
+	Operator string
+	Operand2 string
+}
+
+func calculate(operand1, operator, operand2 string) string {
+
+	n1, _ := strconv.Atoi(strings.Split(operand1, "\r\n")[0])
+	n2, _ := strconv.Atoi(strings.Split(operand2, "\r\n")[0])
+
+	fmt.Println(n1 + n2)
+	var res int
+	if operator == "+\r\n" {
+		res = n1 + n2
+	} else if operator == "-\r\n" {
+		res = n1 - n2
+	} else if operator == "*\r\n" {
+		res = n1 * n2
+	} else if operator == "/\r\n" {
+		res = n1 / n2
+	} else {
+		fmt.Println("Error in input")
+		return ""
+	}
+
+	return strconv.Itoa(res)
+
+}
+
+func prepareRespond(w http.ResponseWriter, r *http.Request) {
+
+	//request, _ := ioutil.ReadAll(r.Body)
+	////request := r.Body
+	//fmt.Println(string(request))
+
+	decoder := json.NewDecoder(r.Body)
+	var t Operation
+	err := decoder.Decode(&t)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(t.Operand1 + t.Operator + t.Operand2)
+	_, _ = w.Write([]byte(calculate(t.Operand1, t.Operator, t.Operand2)))
+
+}
+func main() {
+	http.HandleFunc("/", prepareRespond)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error Occurred!")
+	}
+}
